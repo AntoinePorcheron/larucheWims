2565f48494a054e634d46b6950f51f728b234085
yan <i@yinyan.fr>
2016-02-18 10:55:24 +0100

    structure?

---
+++ essayer/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yan on 16/2/18.
+ */
+
+/**
+ * Le principale et seule variable globale de notre partie de projet
+ */
+var wimsGraph = {};
+
+/**
+ * un link simple ver wimsGraph
+ */
+var wg = wimsGraph;
+
+wg.ui = {};
+wg.default = {};
+wg.options = {};
+
+wg.class = {};
---
+++ essayer/toolbar.js
@@ -0,0 +1,32 @@
+/**
+ * Created by yan on 16/2/18.
+ */
+
+
+wg.ui.$toolbar = (/**
+ * gerer le toolbar
+ * @param tools, list de wg.class.tool
+ * @param option, options par default wg.default.toolbar.option
+ * @returns {*|jQuery|HTMLElement}
+ */
+    function (tools, option) {
+    tools = tools || wg.default.toolbar.tools;
+    option = option || wg.default.toolbar.option;
+    var $toolbar = $("<div></div>");
+    var uiList = [];
+    var ui = function (toolList) {
+        var b = toolList[0];
+        if (b) {
+            uiList.push($("<button></button>")
+                .data("name", b.name)
+                .html($("<img />").attr("src", b.icon))
+                .css(option.buttoncss)
+                .click(b.callback)
+                .appendTo($toolbar));
+            ui(toolList.slice(1))
+        }
+    };
+    ui(tools);
+    return $toolbar
+})();
+
