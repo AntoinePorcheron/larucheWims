78f0076d65b090decf8725d5d52e126b262e9731
yan <i@yinyan.fr>
2016-03-29 11:20:16 +0200

    add image

--- essayer/menu/menu.js
+++ essayer/menu/menu.js
@@ -2,6 +2,64 @@
  * Created by yan on 16/3/8.
  */
 
+var fillImageIntoPoint = function (board, url, paint) {
+    //console.log(url);
+    function getCoord(paint) {
+        return paint.coords.usrCoords
+    }
+    function getCoord2D(paint) {
+        return getCoord(paint).slice(1)
+    }
+
+    function getSize(paint) {
+        return paint.visProp.size
+    }
+
+    board.options.layer["image"] = 10;
+    // make the priority of image higher than point
+    var coodsToPixel = 45;
+    //TODO to have a exact ratio
+    var width = getSize(paint) / coodsToPixel;
+    var point = (function () {
+        var point = getCoord2D(paint);
+        point[0] -= width / 2;
+        point[1] -= width / 2;
+        return point
+    })();
+    return board.create("image", [url, point, [width, width]])
+};
+
+var popupImageUploader = function (board, paint) {
+    if (!FileReader) throw "A Newer Version of Browser is Required.";
+    var $input = $("<input />")
+        .attr("type", "file");
+    var $img = $("<img />").attr({
+        src: "",
+        alt: "image"
+    });
+    /*var $div = $("<div></div>").css({
+            position: "absolute"
+        })
+        .html($input)
+        //.append($img)
+        .appendTo("body");
+        */
+    $input.trigger("click");
+    function readFile(file) {
+        var reader = new FileReader();
+        reader.onload = readSuccess;
+        function readSuccess(event) {
+            //$img.attr("src", event.target.result);
+            //$div.hide();
+            fillImageIntoPoint(board, event.target.result, paint)
+        }
+        reader.readAsDataURL(file);
+    }
+    $input.get(0).onchange = function (event) {
+        readFile(event.srcElement.files[0])
+    }
+};
+
 $(document).ready(function () {
 
 
@@ -9,7 +67,7 @@ $(document).ready(function () {
     //[-5, 4, 5, -4] sont les points haut gauche et bas droite de la grille
 
     //point-------------------------------------------------------------------------------
-    var p1 = board.create('point', [1, 1], {name: 'point1', size: 2, face: 'o', fixed: true});
+    var p1 = board.create('point', [1, 1], {name: 'point1', size: 100, face: 'o', fixed: true, color: "green"});
 
     /**
      *{face:'o'} //  circle,
@@ -42,23 +100,36 @@ $(document).ready(function () {
     var txt = board.create('text', [5, -1, 'testcharactere'], {fontSize: 20});
 
 
-    var initMenu = function (options) {
+    var initMenu = function (option) {
         return {
             callback: function (key, options) {
-                var m = "clicked: " + key;
-                window.console && console.log(m) || alert(m);
+                var l = board.getAllUnderMouse();
+                var i = 0;
+                switch (key) {
+                    case "ajoute":
+                        for(i = 0; i < l.length; i++){
+                            if(l[i].elType == "point"){
+                                popupImageUploader(board, l[i])
+                            }
+                        }
+                        break;
+                    case "sup":
+                        for(i = 0; i < l.length; i++){
+                            if(l[i].elType == "image"){
+                                board.removeObject(l[i])
+                            }
+                        }
+                        break;
+                }
             },
             items: {
-                "name": {name: options.name, icon: options.icon},
+                "ajoute": {name: "Ajoute un image", icon: option.icon},
+                "sup": {name: "supprimer un image", icon: "delete"},
                 "cut": {name: "Cut", icon: "cut"},
                 "copy": {name: "Copy", icon: "copy"}
             }
         };
     };
-
-    $("div").click(function () {
-        console.log(board.getAllUnderMouse())
-    });
 
     $.contextMenu({
         selector: 'div',
@@ -68,7 +139,8 @@ $(document).ready(function () {
             // e is the original contextmenu event, containing e.pageX and e.pageY (amongst other data)
             return initMenu({
                 name: board.getAllUnderMouse()[0].htmlStr,
-                icon: "edit"
+                icon: "edit",
+                element: board.getAllUnderMouse([0])
             })
         }
         /*
