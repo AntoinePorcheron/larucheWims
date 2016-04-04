bcf5a72542ba59ef7a89ff395b22430f325eec78
JulienManga <manga.julien@gmail.com>
2016-04-04 16:03:17 +0200

    Gestion du drag and drop multisaut

--- scripts/classes/TypeBlocPrepa/codeLibrePrepa.js
+++ scripts/classes/TypeBlocPrepa/codeLibrePrepa.js
@@ -43,77 +43,89 @@ function CodeLibrePrepa(numero)
     		});
 
    		liste.addEventListener('drop', function(e) {
-        	var nomZoneIn=" "; //on va récupérer l'id du bloc reçu. 
-        	nomZoneIn=e.dataTransfer.getData('text/plain'); // Affiche le contenu du type MIME « text/plain »
-        	console.log('Données reçu : ' + nomZoneIn);
-        	//Maintenant nous allons faire en sorte de changer de place le bloc si on passe sur le bloc avant ou après lui
-
-        	var id_drop = document.querySelector('#'+nomZoneIn);
-        	//var li = buttonHaut.parentNode.parentNode;
-
-        	// On va gérer le précédent
-        	var previous = id_drop.previousElementSibling;//l'élément précédent le bloc droppé
+        /*Cette fonction sert à décrire ce qui se passera pour le bloc ciblé ce qui se passera lorsqu'on lachera un objet droppable sur lui */
         
-        	var next = id_drop.nextElementSibling;//l'élément suivant le bloc droppé
+        var nomZoneIn=" "; //on va récupérer l'id du bloc reçu. 
+        nomZoneIn=e.dataTransfer.getData('text/plain'); // Affiche le contenu du type MIME « text/plain »
+        console.log('Données reçu : ' + nomZoneIn);
+        //Maintenant nous allons faire en sorte de changer de place le bloc si on passe sur le bloc avant ou après lui
 
-        	if (this==previous) {
-            		if (previous) {
-              			console.log('Un bloc precedent a été trouvé ! Changement...');
-                		id_drop.parentNode.insertBefore(id_drop, previous);
-                		var nom = id_drop.id.slice("RidPrBloc_".length,id_drop.id.length);
-            
-                		var ind = rucheSys.rechercheIndice(nom,rucheSys.listeBlocPrepa);
-            
-                		var temp = rucheSys.listeBlocPrepa[ind];
-                		rucheSys.listeBlocPrepa[ind] = rucheSys.listeBlocPrepa[ind-1];
-                		rucheSys.listeBlocPrepa[ind-1] = temp;
-            		}
-            		else
-            		{
-                		console.log('Pas de précédent, désolé !');
-        		 }
-        	}
-        	else if(this==next)
-        	{
+        var id_drop = document.querySelector('#'+nomZoneIn);
+        //var li = buttonHaut.parentNode.parentNode;
 
-            		//Maintenant on s'occupe du suivant xcv
+        // On va gérer le précédent
+        var previous = id_drop.previousElementSibling;//l'élément précédent le bloc droppé
         
-            		if (next) {
-                		//console.log('Un bloc suivant a été trouvé ! Changement...'+previous.id);
-                		next = next.nextElementSibling;
-                		var nom = id_drop.id.slice("RidPrBloc_".length,id_drop.id.length);
+        var next = id_drop.nextElementSibling;//l'élément suivant le bloc droppé
+
+        var lgNext= Essaim.prototype.trouverSuivant(id_drop,this); //Permet de donner à cb de cases se trouve le bloc ciblé wxc
+        var lgPrev=0;
+        
+
+
+        var lgPrev= Essaim.prototype.trouverPrecedent(id_drop,this);
+        //var actu= id_drop;
+        if(lgNext>0)
+        {
             
-                		var ind = rucheSys.rechercheIndice(nom,rucheSys.listeBlocPrepa);
+           
+            for (var i = 0; i < lgNext; i++) { //on fait faire au bloc droppé lgNext descentes vers le bas.
+                
+                if(next){
+                    next = next.nextElementSibling;
+
+                    console.log('Un bloc suivant a été trouvé ! Changement...');
+                    id_drop.parentNode.insertBefore(id_drop, next);
+                    var nom = id_drop.id.slice("RidPrBloc_".length,id_drop.id.length);
+                
+                    var ind = rucheSys.rechercheIndice(nom,rucheSys.listeBlocPrepa);
+                
+                    var temp = rucheSys.listeBlocPrepa[ind];
+                    rucheSys.listeBlocPrepa[ind] = rucheSys.listeBlocPrepa[ind+1];
+                    rucheSys.listeBlocPrepa[ind+1] = temp;
+
+
+                }
+                else
+                {
+                    console.log("Fin du next");
+                }
+
+                //On change visuellement la place. 
+                
+            }
+        }
+
+        if (lgPrev) {
+            for(var j=0; j< lgPrev;j++)
+            {
+                if (previous) {
+                    console.log('Un bloc precedent a été trouvé ! Changement...');
+                    id_drop.parentNode.insertBefore(id_drop, previous);
+                    var nom = id_drop.id.slice("RidPrBloc_".length,id_drop.id.length);
+                
+                    var ind = rucheSys.rechercheIndice(nom,rucheSys.listeBlocPrepa);
+                
+                    var temp = rucheSys.listeBlocPrepa[ind];
+                    rucheSys.listeBlocPrepa[ind] = rucheSys.listeBlocPrepa[ind-1];
+                    rucheSys.listeBlocPrepa[ind-1] = temp;
+                    previous = id_drop.previousElementSibling;
+                }
+                else
+                {
+                    console.log('Pas de précédent, désolé !');
+                }
+            }
+        }
+        
+        else
+        {
+            console.log('Ni suivant, ne précédent !***********************');
+        }
             
-                		var temp = rucheSys.listeBlocPrepa[ind];
-                		rucheSys.listeBlocPrepa[ind] = rucheSys.listeBlocPrepa[ind+1];
-                		rucheSys.listeBlocPrepa[ind+1] = temp;
-            		}
-            		else
-            		{
-                		console.log('Pas de bloc suivant ici !');
-            		}
+        });
 
-            		id_drop.parentNode.insertBefore(id_drop, next);
-        	}
-        	else
-		{
-            		console.log('Ni suivant, ne précédent !***********************');
-		}
-            	console.log(this.id);
-
-
-
-
-
-
-
-        	//nomBoutton='#'+nomBoutton;
-		 //buttonDD=document.querySelector(nomBoutton);
-		 //buttonDD.onclick(); // On va simuler l'activation du bouton
-		 });
-
-		 /* Fin des modifs */
+    /* Fin des modifs */
 		
 		
 		var barre_tache = document.createElement("DIV");
