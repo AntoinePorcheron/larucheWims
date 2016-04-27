a9a3e33833b83ee170df25c3ff0c793cb91413b3
Seolart <eloame.emblem@wanadoo.fr>
2016-04-27 17:46:54 +0200

    Commentaires + arrow

--- scripts/classes/TypeEssaim/essaimDessinJSXGraph.js
+++ scripts/classes/TypeEssaim/essaimDessinJSXGraph.js
@@ -10,8 +10,8 @@ var GLOB_ligne = "line";
 var GLOB_cercle = "circle";
 var GLOB_arrow = "arrow";
 var GLOB_segment = "segment";
-
 var GLOB_axe = "axis";
+
 EssaimJSXGraph = function (num) {
 
     //Appelle le constructeur parent
@@ -35,7 +35,7 @@ EssaimJSXGraph = function (num) {
 EssaimJSXGraph.prototype = Object.create(Essaim.prototype);
 EssaimJSXGraph.prototype.constructor = EssaimJSXGraph;
 
-//Définit les nouveaux attributs
+//Définition des nouveaux attributs
 EssaimJSXGraph.prototype.nomAffiche = "Essaim : Dessin JSXGraph";
 EssaimJSXGraph.prototype.proto = "EssaimJSXGraph";
 
@@ -73,6 +73,7 @@ EssaimJSXGraph.prototype.initEnonce = function () {
     tab.appendChild(li);
 }
 
+/* Création d'un bloc */
 EssaimJSXGraph.prototype.creerBloc = function (dataRecup) {
     Essaim.prototype.initBloc.call(this);
 
@@ -151,23 +152,24 @@ EssaimJSXGraph.prototype.creerBloc = function (dataRecup) {
 	/* title permet d'afficher une infobulle au survol du bouton */
     var $button_switch_grille = $("<button title=\"Affiche/enlève la grille.\">Grille</button>").appendTo($div_button_retour_chariot_Action).click(
 	{essaimJSXGraph : this}, function(event){
-	    event.data.essaimJSXGraph.grid = !event.data.essaimJSXGraph.grid;
-	    if (event.data.essaimJSXGraph.grid){
+		event.data.essaimJSXGraph.grid = !event.data.essaimJSXGraph.grid;
+		if (event.data.essaimJSXGraph.grid){
 		event.data.essaimJSXGraph.brd.removeGrids();	
-	    }else{
+		}
+		else {
 		event.data.essaimJSXGraph.brd.create('grid', []);
-	    }
-	    event.data.essaimJSXGraph.brd.fullUpdate();
+		}
+		event.data.essaimJSXGraph.brd.fullUpdate();
 	});
 	
 	var $button_libre = $("<button title=\"Permet de déplacer des objets dans le graphe.\"> Deplacer </button>").appendTo($div_button_retour_chariot_Action).click(
 	{essaimJSXGraph : this}, function(event){
-	    event.data.essaimJSXGraph.mode = GLOB_libre
+		event.data.essaimJSXGraph.mode = GLOB_libre
 	});
 	
 	var $menu_deroulant = $("<select></select>");
 	
-	var $save = $("<button title=\"Permet de sauvegarder des éléments du graphique dans une boite à dessin.\">Ajout dans boîte à dessins</button>").appendTo($div_button_retour_chariot_Action).click(
+	var $save = $("<button title=\"Permet de sauvegarder des éléments du graphique dans une boite à dessin.\">Ajout dans boîte à dessin </button>").appendTo($div_button_retour_chariot_Action).click(
 	{essaimJSXGraph : this, menu_D : $menu_deroulant}, function(event){
 	    var tab = {};
 		if(event.data.essaimJSXGraph.menu_enregistre){
@@ -225,15 +227,15 @@ EssaimJSXGraph.prototype.creerBloc = function (dataRecup) {
             event.data.essaimJSXGraph.mode = GLOB_segment;
         });
 
-    var $button_arrow = $("<button title=\"Permet de créer un segment. On utilise deux points pour cela.\">Vecteur</button>").appendTo($div_button_retour_chariot_Objet).click(
-	{essaimJSXGraph : this}, function(event){
-	    event.data.essaimJSXGraph.mode = GLOB_arrow;
-	});
+    var $button_arrow = $("<button title=\"Permet de créer un vecteur. On utilise deux points pour cela.\">Vecteur</button>").appendTo($div_button_retour_chariot_Objet).click(
+		{essaimJSXGraph : this}, function(event){
+			event.data.essaimJSXGraph.mode = GLOB_arrow;
+		});
 
     var $button_axis = $("<button title=\"Permet de créer un axe. On utilise deux points pour cela.\">Axe</button>").appendTo($div_button_retour_chariot_Objet).click(
-	{essaimJSXGraph : this}, function(event){
-	    event.data.essaimJSXGraph.mode = GLOB_axe;
-	});
+		{essaimJSXGraph : this}, function(event){
+			event.data.essaimJSXGraph.mode = GLOB_axe;
+		});
 	
     /* Permet de créer un axe en donnant les coordonnées des points dans une zone de texte */
 	
@@ -282,45 +284,51 @@ EssaimJSXGraph.prototype.creerBloc = function (dataRecup) {
     /*Creation de points, à retoucher/améliorer*/
     var essaimJSXGraph = this;
     this.brd.on('up', function(event){
-	if (essaimJSXGraph.mode !== GLOB_libre){
-	    var point = undefined;
-	    var brd = essaimJSXGraph.brd;
-	    if (brd.drag_dx !== 0 || brd.drag_dy !== 0){
-		essaimJSXGraph.point = [];
-	    }else{
-		var getMouseCoords = function(event) {
-		    var cPos = brd.getCoordsTopLeftCorner(event,0),
-			absPos = JXG.getPosition(event, 0),
-			dx = absPos[0]-cPos[0],
-			dy = absPos[1]-cPos[1];
-		    return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], brd);
+		if (essaimJSXGraph.mode !== GLOB_libre){
+			var point = undefined;
+			var brd = essaimJSXGraph.brd;
+			if (brd.drag_dx !== 0 || brd.drag_dy !== 0){
+				essaimJSXGraph.point = [];
+				}
+				else{
+					var getMouseCoords = function(event) {
+						var cPos = brd.getCoordsTopLeftCorner(event,0),
+							absPos = JXG.getPosition(event, 0),
+							dx = absPos[0]-cPos[0],
+							dy = absPos[1]-cPos[1];
+						return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], brd);
+					}
+				
+				var parent = undefined;
+				var coords = getMouseCoords(event);
+		
+				for (element in brd.objects){
+					if (JXG.isPoint(brd.objects[element])
+					&& brd.objects[element].hasPoint(coords.scrCoords[1],coords.scrCoords[2])){
+						point = element;
+					}
+				}
+				if (point === undefined){
+					point = brd.create("point", brd.getUsrCoordsOfMouse(event));
+					if (parent !== undefined){
+						point.ancestors[0] = 0;
+					}
+				}
+				else if (!brd.objects[point].getAttribute("visible")){
+					brd.objects[point].setAttribute({visible : true});
+				}
+				essaimJSXGraph.point.push(point);
+			}
+		
+			/*Création de la forme souhaitée*/
+			if (essaimJSXGraph.mode === GLOB_point){
+				essaimJSXGraph.point = [];
+			}
+			else if (essaimJSXGraph.point.length === 2){
+				brd.create(essaimJSXGraph.mode, essaimJSXGraph.point);
+				essaimJSXGraph.point = [];
+			}
 		}
-		var parent = undefined;
-		var coords = getMouseCoords(event);
-		for (element in brd.objects){
-		    if (JXG.isPoint(brd.objects[element])
-			&& brd.objects[element].hasPoint(coords.scrCoords[1],coords.scrCoords[2])){
-			point = element;
-		    }
-		}
-		if (point === undefined){
-		    point = brd.create("point", brd.getUsrCoordsOfMouse(event));
-		    if (parent !== undefined){
-			point.ancestors[0] = 0;
-		    }
-		}else if (!brd.objects[point].getAttribute("visible")){
-		    brd.objects[point].setAttribute({visible : true});
-		}
-		essaimJSXGraph.point.push(point);
-	    }
-	    /*Création de la forme souhaitée*/
-	    if (essaimJSXGraph.mode === GLOB_point){
-		essaimJSXGraph.point = [];
-	    }else if (essaimJSXGraph.point.length === 2){
-		brd.create(essaimJSXGraph.mode, essaimJSXGraph.point);
-		essaimJSXGraph.point = [];
-	    }
-	}
     });
 }
 
@@ -334,75 +342,128 @@ EssaimJSXGraph.prototype.detruitBloc = function () {
     Essaim.prototype.detruitBloc.call(this);
 }
 
+/* Changer le nom de certaines variables */
 EssaimJSXGraph.prototype.toOEF = function(){
-    var x1 = this.brd.getBoundingBox()[0];
-    var y1 = this.brd.getBoundingBox()[1];
-    var x2 = this.brd.getBoundingBox()[2];
-    var y2 = this.brd.getBoundingBox()[3];
+    var bord_gauche = this.brd.getBoundingBox()[0];		// remplacer x1 par bord_gauche
+    var bord_haut = this.brd.getBoundingBox()[1];		// remplacer y1 par bord_haut
+    var bord_droit = this.brd.getBoundingBox()[2];		// remplacer x2 par bord_droit
+    var bord_bas = this.brd.getBoundingBox()[3];		// remplacer y2 par bord_bas
 
     console.log(this.brd.getBoundingBox());
 
-    var OEF = "\\text{rangex" + this.nom + " = " + x1 + "," + x2 + "}\n"
-    OEF += "\\text{rangey" + this.nom + " = " + y2 + "," + y1 + "}\n";
+    var OEF = "\\text{rangex" + this.nom + " = " + bord_gauche + "," + bord_droit + "}\n"
+    OEF += "\\text{rangey" + this.nom + " = " + bord_bas + "," + bord_haut + "}\n";
     OEF += "\\text{" + this.nom + " = rangex \\rangex" + this.nom + "\n";
     OEF += "rangey \\rangey" + this.nom + "\n";
     
     if (!this.grid){
-	console.log(JXG.Options.axis.ticks.ticksDistance);
-	for (var i = 0; i < this.brd.grids.length; i++){
-	    console.log(this.brd.grids[i]);
-	}
+		console.log(JXG.Options.axis.ticks.ticksDistance);
+		
+		for (var i = 0; i < this.brd.grids.length; i++){
+			console.log(this.brd.grids[i]);
+		}
     }
 
     
     for (element in this.brd.objects){
-	var brdElement = this.brd.objects[element];
-	if (brdElement.getAttribute("visible")){
-	    switch (brdElement.getType()){
-	    case GLOB_point :
-		OEF +=  "point " + brdElement.X() + "," + brdElement.Y() + ",black\n";
-		break;
-	    case GLOB_ligne :
-		var p1 = brdElement.point1;
-		var p2 = brdElement.point2;
-
-		var hauteur = x2 - x1;
-		var largeur = y1 - y2;
+		var brdElement = this.brd.objects[element];
+		
 		/*On recupère la hauteur et la largeur de la zone de dessin, en terme de 
-		  coordonnées du dessin*/
+		coordonnées du dessin*/
+		var largeur = bord_droit - bord_gauche;
+		var hauteur = bord_haut - bord_bas;
 		
-		var coef = Math.sqrt(largeur * largeur + hauteur * hauteur);
-		/*Taille de la diagonale de la zone de dessin*/
-		
-		var a = (p2.X() - p1.X()) * coef;
-		var b = (p2.Y() - p1.Y()) * coef;
-		/*a et b correspondent au vecteur de translation des deux points qui correspondent à
-		  la ligne, car en OEF la primitive ligne ne fait qu'un segment, on translate donc 
-		  les points en dehors de la zone de dessin, pour donner l'illusion d'une droite*/
-		OEF += "line " +
-		    (a + p1.X()) + "," + (b + p1.Y()) + "," +
-		    (-a + p2.X()) + "," + (-b + p2.Y()) +
-		    ",black\n";
+		if (brdElement.getAttribute("visible")){
+			switch (brdElement.getType()){
+				case GLOB_point :
+					OEF +=  "point " + brdElement.X() + "," + brdElement.Y() + ",black\n";
+					break;
+				case GLOB_ligne :
+					var p1 = brdElement.point1;
+					var p2 = brdElement.point2;
+					
+				
+				
+				/*Taille de la diagonale de la zone de dessin*/
+					var coef = Math.sqrt(largeur * largeur + hauteur * hauteur);
+				
+				/*a et b correspondent aux vecteurs de translation des deux points qui correspondent à
+				la ligne, car en OEF la primitive ligne ne fait qu'un segment, on translate donc 
+				les points en dehors de la zone de dessin, pour donner l'illusion d'une droite*/
+					var a = (p2.X() - p1.X()) * coef;
+					var b = (p2.Y() - p1.Y()) * coef;
+				
+					OEF += "line " +
+						(a + p1.X()) + "," + (b + p1.Y()) + "," +
+						(-a + p2.X()) + "," + (-b + p2.Y()) +
+						",black\n";
+					break;
+				case GLOB_cercle :
+					OEF += "circle " + brdElement.center.X() + "," + brdElement.center.Y() +
+						"," + (brdElement.Radius() * this.brd.unitX) + ",black\n";
+					break;
+				case GLOB_segment :
+					var p1 = brdElement.point1;
+					var p2 = brdElement.point2;
 
-		break;
-	    case GLOB_cercle :
-		OEF += "circle " + brdElement.center.X() + "," + brdElement.center.Y() +
-		    "," + (brdElement.Radius() * this.brd.unitX) + ",black\n";
-		break;
-	    case GLOB_segment :
-		var p1 = brdElement.point1;
-		var p2 = brdElement.point2;
-
-		tmp = "segment " +
-		    p1.X() + "," + p1.Y()+ "," +
-		    p2.X() + "," + p2.Y() + ",black\n";
-		console.log(tmp);
-		OEF += tmp;
-		break;
-	    default:
-		break;
-	    }
-	}	
+					tmp = "segment " +
+						p1.X() + "," + p1.Y()+ "," +
+						p2.X() + "," + p2.Y() + ",black\n";
+					OEF += tmp;
+					break;
+				case GLOB_arrow : 
+					var p1 = brdElement.point1;
+					var p2 = brdElement.point2;
+					
+					tmp = "arrow " +
+						p1.X() + "," + p1.Y()+ "," +
+						p2.X() + "," + p2.Y() + ",7,black\n";
+					OEF += tmp;
+					break;
+					
+					/*  En cours. Voir tableau 
+				case GLOB_axe : 
+				 p1.X() permet de récupérer la coordonnée X du point p1 
+					
+					
+					var p1 = brdElement.point1;
+					var p2 = brdElement.point2;
+					
+					var distance_largeur_gauche; 	// on récupère la distance avec le bord gauche
+					var distance_largeur_droite; 	// on récupère la distance avec le bord droit
+					var distance_hauteur_haut;		// on récupère la distance avec la hauteur haute
+					var distance_hauteur_bas;		// on récupère la distance avec la hauteur basse
+					
+				 On cherche à savoir quel point est au dessus 
+				if (p1.X() > p2.X()){
+					distance_largeur_droite = Math.sqrt(largeur * largeur + p1.X() * p1.X());
+					distance_largeur_gauche = Math.sqrt(largeur * largeur + p2.X() * p2.X());
+				} 
+				else{
+					distance_largeur_gauche = Math.sqrt(largeur * largeur + p2.X() * p2.X());
+					distance_largeur_droite = Math.sqrt(largeur * largeur + p1.X() * p1.X());
+				}
+				
+				On cherche à savoir quel point est à droite 
+				if (p1.Y() > p2.Y()){
+					distance_hauteur_haut = Math.sqrt(hauteur * hauteur + p1.Y() * p1.Y());
+					distance_hauteur_bas = Math.sqrt(hauteur * hauteur + p2.Y() * p2.Y());
+				} 
+				else {
+					distance_hauteur_bas = Math.sqrt(hauteur * hauteur + p2.Y() * p2.Y());
+					distance_hauteur_haut = Math.sqrt(hauteur * hauteur + p1.Y() * p1.Y());
+				}
+				
+				a et b correspondent aux vecteurs de translation des deux points 
+				while 
+					var a = (p2.X() - p1.X());
+					var b = (p2.Y() - p1.Y());
+				
+					break;*/
+				default:
+					break;
+			}
+		}	
     }
     OEF += "hline black,0,0\nvline black,0,0}\n"
     OEF += "\\text{url" + this.nom + " = draw(200,200\n\\" + this.nom + ")}"
